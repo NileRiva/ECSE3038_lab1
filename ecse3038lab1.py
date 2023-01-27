88cc8a8d433ba3fc1ab7c6a5ff992fc6d62e33ec
NileRiva <niletributary@gmail.com>
2023-01-27 14:42:55 -0500

    Wrote, tested and debugged potential_divider().

--- ecse3038lab1.py
+++ ecse3038lab1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def parallel(arr):
     R=0
     for r in arr:
@@ -7,3 +9,16 @@ def parallel(arr):
     return 1/R
 
 parallel([330, 1000, 2200])
+
+def potential_divider(v,arr):
+    Rtotal=0
+    voltarr=[]
+    for R in arr:
+        Rtotal+=R
+    for R in arr:
+        voltarr.append((R/Rtotal)*v)
+    for V in voltarr:
+        print("\"%.2fv\"" % (V))
+    return voltarr
+
+potential_divider(9, [3000, 1000])
